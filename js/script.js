6eb06d332409b6f2339a478020735a6122f226a7
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-15 19:46:07 +0530

    B-1L-55 javasript-naming-convention

---
+++ js/script.js
@@ -0,0 +1,209 @@
+// JavaScript naming conventions
+// camelCase
+
+// 1. Function Defination
+function myFunctionA(a1){
+    // Function Body
+    console.log("Hello "+ a1);
+}
+// 2. Function Calling/Invoking
+myFunctionA('everyone...!');
+
+// 1. Function defination
+function myFunctionB(){
+    // Function Body
+    console.log("To Kaise hai aaplog...?");
+}
+myFunctionB();
+
+// 1. Function defination
+let myFunctionC=(a2)=>{
+    // function body
+    console.log('Too Chalye suru karte hai bina kise ' + a2 );
+}
+// 2. function calling
+myFunctionC('deree ke');
+
+// Function Defination
+function myFunctionD(a3){
+    //function body
+    console.log("A for "+ a3);
+}
+// function calling
+myFunctionD("Apple");
+
+// Function defination
+function myFunctionE(a4){ // "a4" is a formal argument
+    // function body
+    console.log('B for '+ a4);
+}
+// function invoking
+myFunctionE(`Bat`); // "bat" is a actual argument
+
+// function defination
+let myFunctionF=(a5)=>{
+    // function body
+    console.log('C for '+ a5);
+}
+// function calling
+myFunctionF("Cat");
+
+// function defination
+function myFunctionG(a6){
+    // function body
+    console.log('D for '+ a6);
+}
+// function invoking
+myFunctionG("Dog");
+
+// Function defination
+function myFunctionH(a7){
+    // function body
+    console.log("E for "+ a7);
+}
+// function calling
+myFunctionH('Eagle');
+
+// function defination
+function myFunctionI(a8){
+    // function body
+    console.log('F for '+ a8);
+}
+// function calling
+myFunctionI("Fox");
+
+// function defination
+function myFunctionJ(a9){
+    // function body
+    console.log('G for '+ a9);
+}
+// function calling
+myFunctionJ("Goat");
+
+// function defination
+let myFunctionK=(a10)=>{
+    // function body
+    console.log("H for "+ a10);
+}
+// function calling
+myFunctionK('Hen');
+
+// Function defination
+function myFunctionL(a11){
+    // function body
+    console.log('I for '+ a11);
+}
+// function calling
+myFunctionL("IceCream");
+
+// function defination
+function myFunctionM(a12){
+    // function body
+    console.log('J for '+ a12);
+}
+// function calling
+myFunctionM("Jocker");
+
+// function defination
+function myFunctionN(a13){
+    // function body
+    console.log(`K for `+ a13);
+}
+// function calling
+myFunctionN('King');
+
+// Function defination
+function myFunctionO(a14){
+    // function body
+    console.log('L for '+ a14);
+}
+// function calling
+myFunctionO("Lion");
+
+// function defination
+function myFunctionP(a15){
+    // function body
+    console.log('M for '+ a15);
+}
+// function calling
+myFunctionP('Mango');
+
+// Function defination
+let myFunctionQ=(a16)=>{
+    // function body
+    console.log("N for "+ a16);
+}
+// function invoke
+myFunctionQ('Nose');
+
+// function defination
+function myFunctionR(a17){
+    // function body
+    console.log('O for '+ a17);
+}
+// function calling
+myFunctionR("Orange");
+
+// function defination
+function myFunctionS(a18){
+    // function body
+    console.log("P for "+ a18);
+}
+// function call
+myFunctionS('Pen');
+
+// function defination 
+function myFunctionT(a19){
+    // function body
+    console.log("Q for "+ a19);
+}
+// function calling
+myFunctionT('Queen');
+
+// function defination
+function myFunctionU(a20){
+    // function body
+    console.log(`R for `+ a20);
+}
+// function invoke
+myFunctionU('Rose');
+
+// function defination
+function myFunctionV(a21){
+    // function body
+    console.log("S for "+ a21);
+}
+// function calling
+myFunctionV(`Sun`);
+
+// function defination
+function myFunctionW(a22){
+    // function body
+    console.log('T for '+ a22);
+}
+// function calling
+myFunctionW(`Tiger`);
+
+// Function defination
+function myFunctionX(a23){
+    // function body
+    console.log("U for "+ a23);
+}
+// function calling
+myFunctionX('Umberella');
+
+// function defination 
+function myFunctionY(a24){
+    // function body
+    console.log("V for "+ a24);
+}
+// function calling
+myFunctionY('Van');
+
+// Function defination 
+function myFunctionZ(a25){
+    // function body
+    console.log("W for "+ a25);
+}
+// function calling
+myFunctionZ(`Wall`);
